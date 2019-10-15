2b902a5b3ee6c7ea73f2efbe2064e9f691fb7141
unknown <WebMan@PC2019002.SKY>
2019-10-15 19:44:00 +0800

    demo5

---
+++ config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// var domain = document.domain.replace(/^\w+\.(\w+\.\w+)$/, '$1')
+// var domain = 'hscp.com'
+ var domain = 'hscp8888.com'
+//var domain = 'hscp.com'
+
+var baseUrl = `//api.${domain}`
+
+var wwwUrl = `http://www.${domain}`
+var mUrl = `//m.${domain}`
+var appDownUrl = ''
